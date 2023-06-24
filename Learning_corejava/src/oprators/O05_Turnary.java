234b0b928266ff003b914f44f0fcbb62d310f9fc
chintan <chintan.tops@gmail.com>
2023-06-24 09:03:23 +0530

    oprators

---
+++ Learning_corejava/src/oprators/O05_Turnary.java
@@ -0,0 +1,16 @@
+package oprators;
+
+public class O05_Turnary {
+	public static void main(String[] args) {
+		
+		
+		//condition ? true : false
+		
+		int a = 100;
+		int b = 200;
+		
+		String c = a>b ? "A is gretear" : "B is greater";
+		
+		System.out.println(c);
+	}
+}
